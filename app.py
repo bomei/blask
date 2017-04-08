01a932a5ebb634f8815d713f93d73746dcbd57c5
Bo <zannb@outlook.com>
2017-04-08 19:28:40 +0800

    增加show_coms接口

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import requests
+import serial.tools.list_ports
 
 app = Flask(__name__)
 
@@ -9,14 +10,23 @@ def hello_world():
     return 'hello world'
 
 
+@app.route('/show_com')
+def show_coms():
+    plist = serial.tools.list_ports.comports()
+    ans = ''
+    for com in plist:
+        ans += '{} - {}<br>'.format(com.device,com.description)
+    return ans
+
+
 def get_my_ip():
     import socket
-    #获取本机电脑名
-    myname = socket.getfqdn(socket.gethostname(  ))
-    #获取本机ip
-    myaddr = socket.gethostbyname(myname)
-    print(myname)
-    print(myaddr)
+    # 获取本机电脑名
+    name = socket.getfqdn(socket.gethostname())
+    # 获取本机ip
+    addr = socket.gethostbyname(name)
+    print(name)
+    print(addr)
 
 
 if __name__ == '__main__':
