713942437b227719bd038ad22440759c0130a222
bobo <zannb@outlook.com>
2017-04-16 00:33:57 +0800

    tornado好用，flask不好用，

--- websocket-client.py
+++ websocket-client.py
@@ -1,7 +1,23 @@
 from websocket import create_connection
-ws=create_connection("ws://localhost:12345")
+ws=create_connection("ws://localhost:12345/chat")
 print('sending hello')
 ws.send('hello')
 result = ws.recv()
 print('[Receive]',result)
+result = ws.recv()
+print('[Receive]',result)
 ws.close()
+#
+# from socketIO_client import SocketIO, LoggingNamespace, BaseNamespace
+#
+#
+# def on_response(*args):
+#     print('on_response',args)
+#
+#
+# # socketIO.emit('my_event', {'data': 'bobo'})
+# # socketIO.on('my_response', on_response)
+# socketIO = SocketIO('localhost')
+# socketIO.on('my_response', on_response)
+# socketIO.emit('my_event',{'data':'hello'}, path='/test')
+# socketIO.wait(seconds=10)
