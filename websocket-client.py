44b9c21a7d5359763c86e98c9eb23a057c26de04
Bo <zannb@outlook.com>
2017-04-17 17:06:31 +0800

    串口和websocket放在一塊

--- websocket-client.py
+++ websocket-client.py
@@ -1,23 +1,56 @@
 from websocket import create_connection
-ws=create_connection("ws://localhost:12345/chat")
-print('sending hello')
-ws.send('hello')
-result = ws.recv()
-print('[Receive]',result)
-result = ws.recv()
-print('[Receive]',result)
-ws.close()
-#
-# from socketIO_client import SocketIO, LoggingNamespace, BaseNamespace
-#
-#
-# def on_response(*args):
-#     print('on_response',args)
-#
-#
-# # socketIO.emit('my_event', {'data': 'bobo'})
-# # socketIO.on('my_response', on_response)
-# socketIO = SocketIO('localhost')
-# socketIO.on('my_response', on_response)
-# socketIO.emit('my_event',{'data':'hello'}, path='/test')
-# socketIO.wait(seconds=10)
+import json
+import serial.tools.list_ports
+import serial
+import time
+import threading
+
+ws = create_connection("ws://zannb.site:9999/chat")
+serial1 = serial.Serial(
+    port='com3',
+    baudrate=9600
+)
+
+
+def serial_send(s):
+    s += '\r\n'
+    s = s.encode()
+    s = list(s)
+    s = [len(s)] + s
+    data = bytes(s)
+    # print(data)
+    print(serial1.write(data))
+
+
+def rev():
+    while 1:
+        time.sleep(0.1)
+        r = ws.recv()
+        print(r)
+        message = json.loads(r)['message']
+        if 'led' in message.lower():
+            serial_send(message)
+
+
+def serial_rx():
+    while 1:
+        time.sleep(0.1)
+        n = serial1.inWaiting()
+        if n > 0:
+            print(n)
+            data = serial1.read(n)
+            try:
+                print('[{}] -> [{}]'.format(data, data.decode('utf-8')))
+            except:
+                print('[EXCEPTION], can\'t decode as utf-8')
+                print(data)
+
+
+t1 = threading.Thread(name='web_rx', target=rev)
+t1.start()
+t2=threading.Thread(name='uart_rx',target=serial_rx)
+t2.start()
+
+while 1:
+    s = input()
+    ws.send(s)
